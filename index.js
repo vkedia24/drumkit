3ff7755709b5da51ee34b1eb3f8e1196eb68e61e
vkedia24 <35229473+vkedia24@users.noreply.github.com>
2021-01-13 13:49:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+var button = document.querySelectorAll("button");
+
+
+for(var i = 0; i < button.length;i++)
+{
+button[i].addEventListener("click",function(){
+  var buttonClicked = this.innerHTML;
+
+   sound(buttonClicked);
+   animation(buttonClicked);
+});
+}
+
+document.addEventListener("keydown",function(keyPressed){
+    sound(keyPressed.key);
+    animation(keyPressed.key);
+});
+
+function sound(key){
+
+  switch (key) {
+    case "w":
+    var tom1 = new Audio("sounds/tom-1.mp3");
+    tom1.play();
+    break;
+
+    case "a":
+    var tom2 = new Audio("sounds/tom-2.mp3");
+    tom2.play();
+    break;
+
+    case "s":
+    var tom3 = new Audio("sounds/tom-3.mp3");
+    tom3.play();
+    break;
+
+    case "d":
+    var tom4 = new Audio("sounds/tom-4.mp3");
+    tom4.play();
+    break;
+
+    case "j":
+    var snare = new Audio("sounds/snare.mp3");
+    snare.play();
+    break;
+
+    case "k":
+    var crash = new Audio("sounds/crash.mp3");
+    crash.play();
+    break;
+
+    case "l":
+    var kick = new Audio("sounds/kick-bass.mp3");
+    kick.play();
+    break;
+
+    default:console.log(buttonClicked);
+  }
+}
+
+function animation(currentKey){
+   var active = document.querySelector("." + currentKey);
+   active.classList.add("pressed");
+
+   setTimeout(function(){
+     active.classList.remove("pressed");
+   },100);
+}
